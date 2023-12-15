2d44bd9dc12131211429c74f025dda4b149d44a7
hohlas <mail@hohla.ru>
2023-12-15 21:56:43 +0300

    normalize update

--- csv_read.py
+++ csv_read.py
@@ -5,7 +5,7 @@ def sort_data(filename):
     with open(filename, 'r') as f:
         reader = csv.reader(f, delimiter=';')
         next(reader)  # Пропускаем строку с заголовком
-        sorted_data = []
+        input_data = []
         empty_cells = []  # Список для хранения информации о пустых ячейках
         for i, row in enumerate(reader, start=2):  # Начинаем с 2, так как пропустили строку с заголовком
             fractals = row[1:]
@@ -15,8 +15,8 @@ def sort_data(filename):
                     empty_cells.append((i, j))  # Добавляем информацию о пустой ячейке
             fractals = [fractal for fractal in fractals if fractal]
             sorted_fractals = sorted(fractals, key=lambda x: int(x.split(':')[0]) if x.split(':')[0] else 0)
-            sorted_data.append([row[0]] + sorted_fractals)
-        print(f"Обработано {len(sorted_data)} строк.")  # Выводим сообщение внутри функции
+            input_data.append([row[0]] + sorted_fractals)
+        print(f"Обработано {len(input_data)} строк.")  # Выводим сообщение внутри функции
         # Выводим информацию о пустых ячейках
         if empty_cells:
             print("Найдены пустые ячейки в следующих местах (строка / столбец):")
@@ -24,30 +24,38 @@ def sort_data(filename):
                 print(cell)
         else:
             print("Отлично, пустых ячеек не найдено.")
-    return sorted_data
+    return input_data
 
-sorted_data = sort_data('Nero_XAUUSD60.csv') # Nero_XAUUSD60.csv    Nero_5.csv
+input_data = sort_data('Nero_5.csv') # Nero_XAUUSD60.csv    Nero_5.csv
 
-# %% нормализация
+# %% преобразование из текста в списки
 from sklearn.preprocessing import MinMaxScaler
 import numpy as np
 import pandas as pd
-# Инициализируем splited_data как копию sorted_data
-splited_data = sorted_data.copy()
 
-for i in range(len(sorted_data)):
-    for j in range(len(sorted_data[i])):
-        # Проверяем, является ли элемент строкой, прежде чем разделять его
-        splited_data[i][j] = np.array(splited_data[i][j].split(':'))
-        
-# Дополнительный код для нормализации
-for row in splited_data:
-    for index in [1, 3, 4, 8, 9, 10]:  # индексы для нормализации
+for i in range(len(input_data)):
+    for j in range(1, len(input_data[i])): # пропускаем первый столбец со временем
+        input_data[i][j] = np.array(input_data[i][j].split(':'))
+# %% нормализация
+for row in input_data:
+    for index in [0, 1, 6, 7, 8, 9, 10]:  # индексы для нормализации
         values = [float(item[index]) for item in row[1:]]  # извлекаем значения для нормализации
         min_val, max_val = min(values), max(values)  # находим минимальное и максимальное значения
         # выполняем нормализацию
         for item in row[1:]:
             item[index] = (float(item[index]) - min_val) / (max_val - min_val) if max_val > min_val else 0.5
-df = pd.DataFrame(splited_data) # Создаем объект DataFrame из массива данных
+        if index == 1: # сохраняем минимальное и максимальное значения для индекса 1
+            min_max_values = [min_val, max_val - min_val]
+    # Нормализация данных с индексами 3 и 4 вместе
+    values_3 = [float(item[3]) for item in row[1:]]
+    values_4 = [float(item[4]) for item in row[1:]]
+    min_val = min(min(values_3), min(values_4))
+    max_val = max(max(values_3), max(values_4))
+    for item in row[1:]:
+        item[3] = (float(item[3]) - min_val) / (max_val - min_val) if max_val > min_val else 0.5
+        item[4] = (float(item[4]) - min_val) / (max_val - min_val) if max_val > min_val else 0.5
+    row.insert(1, min_max_values) # Добавляем минимальное и максимальное значения в input_data после столбца с временем
+df = pd.DataFrame(input_data) # Создаем объект DataFrame из массива данных
 df.to_csv('normalized.csv', index=False) # Сохраняем DataFrame в файл normalized.csv            
 # %% old code
+# %% нормализация
