c6705456cf6c9fb689ff83a72bf05664ef85a4fc
hohlas <mail@hohla.ru>
2023-12-16 22:51:44 +0300

    докрутил

--- csv_read.py
+++ csv_read.py
@@ -1,6 +1,8 @@
-# %% считывание из файла csv
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+import pandas as pd
 import csv
-
+# %% считывание из файла csv в строковый формат с разделителями ':'
 def read_data(filename):
     with open(filename, 'r') as f:
         reader = csv.reader(f, delimiter=';')
@@ -16,39 +18,44 @@ def read_data(filename):
             fractals = [fractal for fractal in fractals if fractal]
             sorted_fractals = sorted(fractals, key=lambda x: int(x.split(':')[0]) if x.split(':')[0] else 0)
             data.append([row[0]] + sorted_fractals)
-        print("из файла ",filename,f" считано {len(data)} строк.")  # Выводим сообщение внутри функции
+        print("from ",filename,f" read {len(data)} lines.")  # Выводим сообщение внутри функции
         # Выводим информацию о пустых ячейках
         if empty_cells:
-            print("Найдены пустые ячейки в следующих местах (строка / столбец):")
+            print("Find empty cells in (row / column):")
             for cell in empty_cells:
                 print(cell)
         else:
-            print("Пустых ячеек не найдено.")
+            print("OK: no empty cells in file ",filename)
     return data
-
-from sklearn.preprocessing import MinMaxScaler
-import numpy as np
-import pandas as pd
-# %% преобразование из текста в списки и нормализация
-def normalize_data(data):
+# %% преобразование из текста с разделителямми ':' в списки 
+def txt_2_list(data):
     for i in range(len(data)):
         for j in range(1, len(data[i])): # пропускаем первый столбец со временем
             data[i][j] = np.array(data[i][j].split(':')).astype(float) # преобразование из текста в списки
-    
+    #keys = ["date"] + ["fr" + str(i) for i in range(1, len(data[0]))]
+    #dict_data = [dict(zip(keys, sublist)) for sublist in data]
+    return data
+#check = read_data('Nero_XAUUSD60.csv') # Nero_XAUUSD60.csv    Nero_5.csv
+#check = txt_2_list(check)
+# %% проверка каждого фрактала "не станет ли он в будущем первым уровнем" и запись статуса в доп. столбец. 
+def find_strong_levels(data):
     for row in data: # в каждой строке в данных 
-        row.insert(1, [0, 0]) # вставляем новый список [0, 0] после 'даты'
-     
-    for i in range(len(data)):
-        for j in range(i + 1, len(data)):
-            for k in range(2, len(data[j])):
-                a=data[i][2][13]; b=data[j][k][13]; strong=data[j][k][5]
-                if data[i][2][13] == data[j][k][13] and data[j][k][5] > 0:
-                    data[i][1][0] = 1
-                    data[i][1][1] = data[j][k][13]
+        row.insert(1, [0, 0]) # вставляем после 'даты' новый список status[0, 0] чтобы записывать туда статус первого уровня
+    # time[0] status[0] fractal[0][0] fractal[0][1] ... fractal[0][n]
+    # time[1] status[1] fractal[1][0] fractal[1][1] ... fractal[1][n]     
+    first_levels=np.zeros(len(data),dtype=int)
+    for i in range(len(data)): # каждую строку
+        for j in range(i + 1, len(data)): # сверяем со следующей и ниже
+            for k in range(2, len(data[j])): # начиная со второго индекса (fractal[j][0])
+                if data[i][2][13] == data[j][k][13] and data[j][k][5] > 0: # время терущего фрактала совпало со временем фрактала из будущего, и тот со статусом "первый"
+                    data[i][1][0] = 1 # записываем статус новому фракталу
+                    data[i][1][1] = data[j][k][13] #
+                    first_levels[i]=1
                     break
-    
-
-     
+    print('find ',sum(first_levels)," first levels")                
+    return data
+# %% нормализация всех данных к диапазону 0..1
+def normalize(data):     
     for row in data:
         for index in [0, 1, 6, 7, 8, 9, 10, 11, 12]:  # индексы для нормализации
             values = [float(item[index]) for item in row[2:]]  # извлекаем значения для нормализации
@@ -67,15 +74,16 @@ def normalize_data(data):
             item[3] = (float(item[3]) - min_val) / (max_val - min_val) if max_val > min_val else 0.5
             item[4] = (float(item[4]) - min_val) / (max_val - min_val) if max_val > min_val else 0.5
         row.insert(1, min_max_values) # Добавляем минимальное и максимальное значения в data после столбца с временем
-    print('данные нормализованы')
-    
+    print('OK: data normalized')
     return data   
  
-csv_data = read_data('Nero_XAUUSD60.csv') # Nero_XAUUSD60.csv    Nero_5.csv
-normalized_data=normalize_data(csv_data)
+check = read_data('Nero_XAUUSD60.csv') # Nero_XAUUSD60.csv    Nero_5.csv
+check = txt_2_list(check)
+check = find_strong_levels(check)
+check = normalize(check)
           
 # %% save to csv
-df = pd.DataFrame(normalized_data) # Создаем объект DataFrame из массива данных
+df = pd.DataFrame(check) # Создаем объект DataFrame из массива данных
 df.to_csv('normalized.csv', index=False) # Сохраняем DataFrame в файл normalized.csv  
 
 # %% нормализация
