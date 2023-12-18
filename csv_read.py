1f8650ec32670e94b194eee44632e07370cab03d
hohlas <mail@hohla.ru>
2023-12-18 21:38:06 +0300

    csv_read.py is ready

--- csv_read.py
+++ csv_read.py
@@ -33,61 +33,69 @@ def read_data(filename):
         else:
             print("OK: no empty cells in file ",filename)
     
+    for row in data: # в каждой строке в данных после 'даты' 
+        row.insert(1, [0, 0]) # вставляем новый столбец predictor[0,0] чтобы записывать туда статус "ключевого" уровня
+        row.insert(1, [0, 0])  # Добавляем новый столбец restore[0,0] для min_max_values
 # после преобразований получили список списков
 # time[0] predictor[0] restore[0] fractal[0][0] fractal[0][1] ... fractal[0][n]
 # time[1] predictor[1] restore[1] fractal[1][0] fractal[1][1] ... fractal[1][n]  
 # ...
 # time[m] predictor[m] restore[m] fractal[m][0] fractal[m][1] ... fractal[m][n]  
  
-# %% проверка каждого фрактала "не станет ли он в будущем первым уровнем" и запись статуса в доп. столбец. 
-
-    for row in data: # в каждой строке в данных 
-        row.insert(1, [0, 0]) # вставляем после 'даты' новый список status[0, 0] чтобы записывать туда статус "ключевого" уровня
-    # time[0] status[0] fractal[0][0] fractal[0][1] ... fractal[0][n]
-    # time[1] status[1] fractal[1][0] fractal[1][1] ... fractal[1][n] 
-    # ...
-    # time[m] status[m] fractal[m][0] fractal[m][1] ... fractal[m][n]         
+# проверка каждого фрактала "не станет ли он в будущем первым уровнем" и запись статуса в доп. столбец.       
     first_levels_counter=np.zeros(len(data),dtype=int)
     for i in range(len(data)): # каждую строку
         for j in range(i + 1, len(data)): # сверяем со следующей и ниже
-            for k in range(2, len(data[j])): # начиная со второго индекса (fractal[j][0])
+            for k in range(3, len(data[j])): # начиная со второго индекса (fractal[j][0])
                 # поиск ближайшего "ключевого" уровня в будущем
-                if data[j][k][5] > 0 and data[i][1][1] ==0: # в будущей истории найден ключевой уровень, и ни один уровень еще не сохранен
-                    data[i][1][1] =  data[j][k][1] # сохраняем значение ближайшего в будущем "ключевого" уровня
+                if data[j][k][5] > 0 and data[i][2][1] ==0: # в будущей истории найден ключевой уровень, и ни один уровень еще не сохранен
+                    data[i][2][1] =  data[j][k][1] # сохраняем значение ближайшего в будущем "ключевого" уровня
                 # проверка, будет ли текущий фрактал в будущем "ключевым уровнем"
-                if data[i][2][13] == data[j][k][13] and data[j][k][5] > 0: # время терущего фрактала совпало со временем фрактала из будущего, и тот со статусом "ключевой"
-                    data[i][1][0] = 1 # говорит о том, что в будущем этот фрактал будет "ключевым уровнем"
+                if data[i][3][13] == data[j][k][13] and data[j][k][5] > 0: # время терущего фрактала совпало со временем фрактала из будущего, и тот со статусом "ключевой"
+                    data[i][2][0] = 1 # говорит о том, что в будущем этот фрактал будет "ключевым уровнем"
                     first_levels_counter[i]=1 # счеткик количества найденных ключевых уровней
                     break
     print('find ',sum(first_levels_counter)," first levels")                 
     
-# %% нормализация всех данных к диапазону 0..1
+# нормализация всех данных к диапазону 0..1
   
     for row in data:
         for index in [0, 1, 6, 7, 8, 9, 10, 11, 12]:  # индексы для нормализации
-            values = [float(item[index]) for item in row[2:]]  # извлекаем значения для нормализации
+            values = [float(item[index]) for item in row[3:]]  # извлекаем значения для нормализации
             min_val, max_val = min(values), max(values) # минимальное и максимальное значения
             # выполняем нормализацию
-            for item in row[2:]:
+            for item in row[3:]:
                 item[index] = (float(item[index]) - min_val) / (max_val - min_val) if max_val > min_val else 0.5
             if index == 1: # минимальное и максимальное значения для индекса 1 (цена)
-                min_max_values = [min_val, max_val - min_val]
+                row[1] = [min_val, max_val - min_val]  # Добавляем минимальное и максимальное значения в data после столбца с временем
         # Нормализация данных с индексами 3 и 4 вместе
-        values_3 = [float(item[3]) for item in row[2:]]
-        values_4 = [float(item[4]) for item in row[2:]]
+        values_3 = [float(item[3]) for item in row[3:]]
+        values_4 = [float(item[4]) for item in row[3:]]
         min_val = min(min(values_3), min(values_4))
         max_val = max(max(values_3), max(values_4))
-        for item in row[2:]:
+        for item in row[3:]:
             item[3] = (float(item[3]) - min_val) / (max_val - min_val) if max_val > min_val else 0.5
             item[4] = (float(item[4]) - min_val) / (max_val - min_val) if max_val > min_val else 0.5
-        row.insert(1, min_max_values) # Добавляем минимальное и максимальное значения в data после столбца с временем
     print('OK: data normalized')
     return data   
  
 check = read_data('Nero_XAUUSD60.csv') # Nero_XAUUSD60.csv    Nero_5.csv
-          
+ # получили список списков
+# time[0] predictor[0] restore[0] fractal[0][0] fractal[0][1] ... fractal[0][n]
+# time[1] predictor[1] restore[1] fractal[1][0] fractal[1][1] ... fractal[1][n]  
+# ...
+# time[m] predictor[m] restore[m] fractal[m][0] fractal[m][1] ... fractal[m][n]           
 # %% save to csv
-df = pd.DataFrame(check) # Создаем объект DataFrame из массива данных
-df.to_csv('normalized.csv', index=False) # Сохраняем DataFrame в файл normalized.csv  
+#df = pd.DataFrame(check) # Создаем объект DataFrame из массива данных
+#df.to_csv('normalized.csv', index=False) # Сохраняем DataFrame в файл normalized.csv  
+
+with open('normalized.csv', 'w', newline='') as f:  # Открываем файл для записи
+    writer = csv.writer(f, delimiter=';')  # Создаем объект writer для записи в CSV-файл
+    for row in check:  # Перебираем каждую строку в данных
+        new_row = [row[0]]  # Время сохраняется без изменений
+        new_row += [':'.join(map(str, item)) for item in row[1:]]  # Преобразуем списки в строки, разделенные ':'
+        writer.writerow(new_row)  # Записываем строку в файл
+
+print(f"Data saved to {'normalized.csv'}")
 
 # %% нормализация
